f177196b9dea46ae4081969109b5a9e4fa2920d4
Nick Pseudo <tomdop.dakar@gmail.com>
2024-07-30 14:04:24 +0200

    adjusted arrows height for carousel, responsive ok

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -48,12 +48,12 @@ export default function Carousel({ pictures }) {
                 {pictures.length > 1 && <>
                     <nav className={styles.carouselNav}>
                         <div className={`${styles.svgContainer} ${isLeftActive ? styles.active : ''}`}>
-                        <svg onClick={handlePrevEvent} width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg onClick={handlePrevEvent} viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M47.0399 7.42497L39.9199 0.344971L0.359863 39.945L39.9599 79.545L47.0399 72.465L14.5199 39.945L47.0399 7.42497Z" fill="white" />
                         </svg>
                         </div>
                         <div className={`${styles.svgContainer} ${isRightActive ? styles.active : ''}`}>
-                        <svg  onClick={handleNextEvent} width="48" height="81" viewBox="0 0 48 81" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg  onClick={handleNextEvent} viewBox="0 0 48 81" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M0.959961 72.9877L8.03996 80.0677L47.64 40.4677L8.03996 0.867676L0.959961 7.94768L33.48 40.4677L0.959961 72.9877Z" fill="white" />
                         </svg>
                         </div>
